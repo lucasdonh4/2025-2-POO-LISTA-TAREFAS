9b71f3d57bf747d13cf64d53779b7d5cd99ee428
lucasdonh4 <l.donha@uni9.edu.br>
2025-10-23 19:33:09 -0300

    Update Program.cs

--- tarfas/Program.cs
+++ tarfas/Program.cs
@@ -1,19 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Preenchendo dados tarefa 01");
-var tarefa01 = new Tarefa();
-tarefa01.Nome = "Fazer compras";
-tarefa01.Descricao  = "Comprar arroz, feijão e frutas";
-tarefa01.DataCriacao = DateTime.Now; 
-tarefa01.Status = 1;
-tarefa01.DataExecucao = null;
+namespace tarfas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var operacoes = new Operacoes();
 
-Console.WriteLine("Dados tarefa 01 preenchidos");
+            Console.WriteLine("=== LISTA DE TAREFAS ===");
+            try
+            {
+                IList<Tarefa> lista = operacoes.Listar();
+                foreach (var tarefa in lista)
+                {
+                    Console.WriteLine($"{tarefa.Id} - {tarefa.Nome} ({tarefa.Status})");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
 
-Console.WriteLine("Inserindo dados no banco de dados");
-
-var operacoes = new Operacoes();
-int idInserido = operacoes.Criar(tarefa01);
-
-Console.WriteLine($"Dados inseridos no banco de dados com sucesso. Id: {idInserido}");
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
